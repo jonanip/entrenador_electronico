517eab19154c3706ac68cc854d1d9f070d00c30e
jonanip <juananip@gmail.com>
2020-06-07 13:53:11 +0200

    cambio de imagenes

--- entrenador_electronico/entrenador/prueba-plantilla.py
+++ entrenador_electronico/entrenador/prueba-plantilla.py
@@ -1,13 +1,15 @@
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets
 from entrenador_electronico.plantillas.plantilla_image import Ui_MainWindow
-
+from PyQt5.QtGui import QIcon, QPixmap
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
-        self.boton_lurra.clicked.connect(self.desactivar_imagen)
+        self.boton_espacio.clicked.connect(lambda: self.cambiar_imagen(ruta="E:\AITA\pycharm\entrenador_electronico\entrenador_electronico\contenido\estacionespacial4.jpg"))
+        self.boton_lurra.clicked.connect(lambda: self.cambiar_imagen(ruta="E:\AITA\pycharm\entrenador_electronico\entrenador_electronico\contenido\cuba1.jpg"))
+
 
     def desactivar_imagen(self):
         if self.imagen.isEnabled():
@@ -15,7 +17,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         else:
             self.imagen.setEnabled(True)
 
-
+    def cambiar_imagen(self, ruta=None):
+        q_pixmap = QPixmap(ruta)
+        self.imagen.setPixmap(q_pixmap)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
