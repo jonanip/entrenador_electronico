9152faa2f32e3d5ebc3a18364d7d77fc79c1722a
jonanip <juananip@gmail.com>
2020-07-14 21:55:33 +0200

    chore: add neopixel test

--- scripts/test_neopixel.py
+++ scripts/test_neopixel.py
@@ -4,7 +4,7 @@ import time
 
 
 if __name__ == "__main__":
-    pixel = neopixel.NeoPixel(pin=board.D18, n=12, pixel_order=neopixel.RGBW)
+    pixel = neopixel.NeoPixel(pin=board.D18, n=12, pixel_order=neopixel.RGB)
     pixel[0] = (10, 0, 0, 0)
     pixel[1] = (10, 0, 0, 0)
     pixel[2] = (10, 0, 0, 0)
