18e2d0d46b187b6d91a9709a66573729f20f5fe1
jonanip <juananip@gmail.com>
2020-07-14 21:20:00 +0200

    chore: add neopixel test

--- scripts/test_neopixel.py
+++ scripts/test_neopixel.py
@@ -5,5 +5,5 @@ import time
 
 if __name__ == "__main__":
     pixel = neopixel.NeoPixel(pin=board.D18, n=5, pixel_order=neopixel.RGBW)
-    pixel[0] = (10, 0, 0, 10)
+    pixel[0] = (150, 0, 0, 10)
     time.sleep(10)
